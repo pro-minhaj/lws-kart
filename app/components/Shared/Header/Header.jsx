4466eca172e418eba0bdf0ed65894a4120e3b80e
pro-minhaj <parsonal322532@gmail.com>
2024-05-19 02:18:49 +0600

    Implement Header Section And Header Items Add

--- app/components/Shared/Header/Header.jsx
+++ app/components/Shared/Header/Header.jsx
@@ -1,48 +1,74 @@
+import Link from 'next/link';
+import Image from 'next/image';
+// import Image 
+import logo from '@/assets/images/logo.svg';
+import { FaHeart, FaUser } from "react-icons/fa6";
+import { FaShoppingCart } from "react-icons/fa";
 
 const Header = () => {
+    // Header Items
+    const headerItems = [
+        {
+            name: "Wishlist",
+            link: "/wishlist",
+            icon: <FaHeart />,
+            count: 8
+        },
+        {
+            name: "Cart",
+            link: "/cart",
+            icon: <FaShoppingCart />,
+            count: 2
+        },
+        {
+            name: "Account",
+            link: "/account",
+            icon: <FaUser />,
+            count: null
+        },
+    ]
     return (
-        <header class="py-4 shadow-sm bg-white">
-            <div class="container flex items-center justify-between">
-                <a href="index.html">
-                    <img src="assets/images/logo.svg" alt="Logo" class="w-32">
-                </a>
+        <header className="py-4 bg-white shadow-sm">
+            <div className="container flex items-center justify-between mx-auto">
+                <Link href="/">
+                    <Image width={150} height={100} src={logo} alt="Logo" priority className="w-32" />
+                </Link>
 
-                <div class="w-full max-w-xl relative flex">
-                    <span class="absolute left-4 top-3 text-lg text-gray-400">
-                        <i class="fa-solid fa-magnifying-glass"></i>
+                <div className="relative flex w-full max-w-xl">
+                    <span className="absolute text-lg text-gray-400 left-4 top-3">
+                        <i className="fa-solid fa-magnifying-glass"></i>
                     </span>
-                    <input type="text" name="search" id="search"
-                        class="w-full border border-primary border-r-0 pl-12 py-3 pr-3 rounded-l-md focus:outline-none hidden md:flex"
-                        placeholder="search">
-                        <button
-                            class="bg-primary border border-primary text-white px-8 rounded-r-md hover:bg-transparent hover:text-primary transition hidden md:flex">Search</button>
+                    <input
+                        type="text"
+                        name="search"
+                        id="search"
+                        className="hidden w-full px-3 py-3 border border-r-0 border-primary rounded-l-md focus:outline-none md:flex"
+                        placeholder="Search..."
+                    />
+                    <button
+                        className="items-center hidden px-8 text-white transition border bg-primary border-primary rounded-r-md hover:bg-transparent hover:text-primary md:flex"
+                    >
+                        Search
+                    </button>
                 </div>
 
-                <div class="flex items-center space-x-4">
-                    <a href="#" class="text-center text-gray-700 hover:text-primary transition relative">
-                        <div class="text-2xl">
-                            <i class="fa-regular fa-heart"></i>
-                        </div>
-                        <div class="text-xs leading-3">Wishlist</div>
-                        <div
-                            class="absolute right-0 -top-1 w-5 h-5 rounded-full flex items-center justify-center bg-primary text-white text-xs">
-                            8</div>
-                    </a>
-                    <a href="#" class="text-center text-gray-700 hover:text-primary transition relative">
-                        <div class="text-2xl">
-                            <i class="fa-solid fa-bag-shopping"></i>
-                        </div>
-                        <div class="text-xs leading-3">Cart</div>
-                        <div
-                            class="absolute -right-3 -top-1 w-5 h-5 rounded-full flex items-center justify-center bg-primary text-white text-xs">
-                            2</div>
-                    </a>
-                    <a href="#" class="text-center text-gray-700 hover:text-primary transition relative">
-                        <div class="text-2xl">
-                            <i class="fa-regular fa-user"></i>
-                        </div>
-                        <div class="text-xs leading-3">Account</div>
-                    </a>
+                <div className="flex items-center gap-5">
+                    {/* Header Items */}
+                    {
+                        headerItems.map((item, index) => <Link key={index} href={`/${item.link}`} className="block">
+                            <div className='flex items-center gap-1 text-center text-gray-700 transition hover:text-primary'>
+                                <span className='text-xl'>
+                                    {item.icon}
+                                </span>
+                                <span className="text-xs leading-3">{item.name}</span>
+                                {
+                                    item.count && <span className="flex items-center justify-center w-5 h-5 text-xs text-white rounded-full bg-primary">
+                                        {item.count}
+                                    </span>
+                                }
+                            </div>
+                        </Link>)
+                    }
                 </div>
             </div>
         </header>
