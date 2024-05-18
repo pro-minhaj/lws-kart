1e7896e8b189351ca8139ca98ad9fb92b9817b3b
pro-minhaj <parsonal322532@gmail.com>
2024-05-19 04:36:56 +0600

    Implement Footer Section Design Complete And Copy Right Section Image Show

--- app/components/Shared/Footer/Footer.jsx
+++ app/components/Shared/Footer/Footer.jsx
@@ -2,6 +2,7 @@ import Image from 'next/image';
 import logo from '@/assets/images/logo.svg';
 import { FaFacebook, FaInstagram, FaTwitter, FaGithub } from "react-icons/fa";
 import Link from 'next/link';
+import methodImg from '@/assets/images/methods.png';
 
 // socialMedia Items
 const socialMedias = [
@@ -28,9 +29,9 @@ const Footer = () => {
     return (
         <>
             <footer className="pt-16 pb-12 bg-white border-t border-gray-100">
-                <div className="container grid grid-cols-3">
-                    <div className="col-span-1 mr-2 space-y-8">
-                        <Image src={logo} alt="logo" width={150} height={50} />
+                <div className="container grid items-start justify-center grid-cols-1 gap-5 mx-auto sm:grid-cols-2 md:grid-cols-3 xl:justify-between xl:grid-cols-5">
+                    <div className="space-y-5">
+                        <Image src={logo} alt="logo" width={180} height={80} />
                         <div className="flex items-center gap-3">
                             {/* Social Media */}
                             {
@@ -42,51 +43,90 @@ const Footer = () => {
                             }
                         </div>
                     </div>
-
-                    <div className="grid grid-cols-2 col-span-2 gap-8">
-                        <div className="grid grid-cols-2 gap-8">
-                            <div>
-                                <h3 className="text-sm font-semibold tracking-wider text-gray-400 uppercase">Solutions</h3>
-                                <div className="mt-4 space-y-4">
-                                    <span className="block text-base text-gray-500 hover:text-gray-900">Marketing</span>
-                                    <span className="block text-base text-gray-500 hover:text-gray-900">Analitycs</span>
-                                    <span className="block text-base text-gray-500 hover:text-gray-900">Commerce</span>
-                                    <span className="block text-base text-gray-500 hover:text-gray-900">Insights</span>
-                                </div>
-                            </div>
-
-                            <div>
-                                <h3 className="text-sm font-semibold tracking-wider text-gray-400 uppercase">Support</h3>
-                                <div className="mt-4 space-y-4">
-                                    <span className="block text-base text-gray-500 hover:text-gray-900">Pricing</span>
-                                    <span className="block text-base text-gray-500 hover:text-gray-900">Documentation</span>
-                                    <span className="block text-base text-gray-500 hover:text-gray-900">Guides</span>
-                                    <span className="block text-base text-gray-500 hover:text-gray-900">API Status</span>
-                                </div>
-                            </div>
-                        </div>
-                        <div className="grid grid-cols-2 gap-8">
-                            <div>
-                                <h3 className="text-sm font-semibold tracking-wider text-gray-400 uppercase">Solutions</h3>
-                                <div className="mt-4 space-y-4">
-                                    <span className="block text-base text-gray-500 hover:text-gray-900">Marketing</span>
-                                    <span className="block text-base text-gray-500 hover:text-gray-900">Analitycs</span>
-                                    <span className="block text-base text-gray-500 hover:text-gray-900">Commerce</span>
-                                    <span className="block text-base text-gray-500 hover:text-gray-900">Insights</span>
-                                </div>
-                            </div>
-
-                            <div>
-                                <h3 className="text-sm font-semibold tracking-wider text-gray-400 uppercase">Support</h3>
-                                <div className="mt-4 space-y-4">
-                                    <span className="block text-base text-gray-500 hover:text-gray-900">Pricing</span>
-                                    <span className="block text-base text-gray-500 hover:text-gray-900">Documentation</span>
-                                    <span className="block text-base text-gray-500 hover:text-gray-900">Guides</span>
-                                    <span className="block text-base text-gray-500 hover:text-gray-900">API Status</span>
-                                </div>
-                            </div>
+                    <div>
+                        <h3 className="text-sm font-semibold tracking-wider text-gray-400 uppercase">Solutions</h3>
+                        <div className="mt-4 space-y-4">
+                            <span className="block text-base text-gray-500 hover:text-gray-900">Marketing</span>
+                            <span className="block text-base text-gray-500 hover:text-gray-900">Analitycs</span>
+                            <span className="block text-base text-gray-500 hover:text-gray-900">Commerce</span>
+                            <span className="block text-base text-gray-500 hover:text-gray-900">Insights</span>
                         </div>
                     </div>
+
+                    <div>
+                        <h3 className="text-sm font-semibold tracking-wider text-gray-400 uppercase">Support</h3>
+                        <div className="mt-4 space-y-4">
+                            <span className="block text-base text-gray-500 hover:text-gray-900">Pricing</span>
+                            <span className="block text-base text-gray-500 hover:text-gray-900">Documentation</span>
+                            <span className="block text-base text-gray-500 hover:text-gray-900">Guides</span>
+                            <span className="block text-base text-gray-500 hover:text-gray-900">API Status</span>
+                        </div>
+                    </div>
+
+                    <div>
+                        <h3 className="text-sm font-semibold tracking-wider text-gray-400 uppercase">Solutions</h3>
+                        <div className="mt-4 space-y-4">
+                            <span className="block text-base text-gray-500 hover:text-gray-900">Marketing</span>
+                            <span className="block text-base text-gray-500 hover:text-gray-900">Analitycs</span>
+                            <span className="block text-base text-gray-500 hover:text-gray-900">Commerce</span>
+                            <span className="block text-base text-gray-500 hover:text-gray-900">Insights</span>
+                        </div>
+                    </div>
+
+                    <div>
+                        <h3 className="text-sm font-semibold tracking-wider text-gray-400 uppercase">Support</h3>
+                        <div className="mt-4 space-y-4">
+                            <span className="block text-base text-gray-500 hover:text-gray-900">Pricing</span>
+                            <span className="block text-base text-gray-500 hover:text-gray-900">Documentation</span>
+                            <span className="block text-base text-gray-500 hover:text-gray-900">Guides</span>
+                            <span className="block text-base text-gray-500 hover:text-gray-900">API Status</span>
+                        </div>
+                    </div>
+
+                    {/* <div className="grid grid-cols-2 col-span-2 gap-8">
+                        <div className="grid grid-cols-2 gap-8">
+                            <div>
+                                <h3 className="text-sm font-semibold tracking-wider text-gray-400 uppercase">Solutions</h3>
+                                <div className="mt-4 space-y-4">
+                                    <span className="block text-base text-gray-500 hover:text-gray-900">Marketing</span>
+                                    <span className="block text-base text-gray-500 hover:text-gray-900">Analitycs</span>
+                                    <span className="block text-base text-gray-500 hover:text-gray-900">Commerce</span>
+                                    <span className="block text-base text-gray-500 hover:text-gray-900">Insights</span>
+                                </div>
+                            </div>
+
+                            <div>
+                                <h3 className="text-sm font-semibold tracking-wider text-gray-400 uppercase">Support</h3>
+                                <div className="mt-4 space-y-4">
+                                    <span className="block text-base text-gray-500 hover:text-gray-900">Pricing</span>
+                                    <span className="block text-base text-gray-500 hover:text-gray-900">Documentation</span>
+                                    <span className="block text-base text-gray-500 hover:text-gray-900">Guides</span>
+                                    <span className="block text-base text-gray-500 hover:text-gray-900">API Status</span>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="grid grid-cols-2 gap-8">
+                            <div>
+                                <h3 className="text-sm font-semibold tracking-wider text-gray-400 uppercase">Solutions</h3>
+                                <div className="mt-4 space-y-4">
+                                    <span className="block text-base text-gray-500 hover:text-gray-900">Marketing</span>
+                                    <span className="block text-base text-gray-500 hover:text-gray-900">Analitycs</span>
+                                    <span className="block text-base text-gray-500 hover:text-gray-900">Commerce</span>
+                                    <span className="block text-base text-gray-500 hover:text-gray-900">Insights</span>
+                                </div>
+                            </div>
+
+                            <div>
+                                <h3 className="text-sm font-semibold tracking-wider text-gray-400 uppercase">Support</h3>
+                                <div className="mt-4 space-y-4">
+                                    <span className="block text-base text-gray-500 hover:text-gray-900">Pricing</span>
+                                    <span className="block text-base text-gray-500 hover:text-gray-900">Documentation</span>
+                                    <span className="block text-base text-gray-500 hover:text-gray-900">Guides</span>
+                                    <span className="block text-base text-gray-500 hover:text-gray-900">API Status</span>
+                                </div>
+                            </div>
+                        </div>
+                    </div> */}
                 </div>
             </footer>
             {/* ./footer */}
@@ -96,7 +136,7 @@ const Footer = () => {
                 <div className="container flex items-center justify-between">
                     <p className="text-white">&copy; TailCommerce - All Right Reserved</p>
                     <div>
-                        <Image src="/assets/images/methods.png" alt="methods" width={50} height={20} />
+                        <Image src={methodImg} className='w-auto h-auto' alt="methods" width={150} height={60} />
                     </div>
                 </div>
             </div>
