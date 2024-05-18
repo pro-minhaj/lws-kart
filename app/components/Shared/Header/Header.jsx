f6b8860a9da51bf25c25de436b7fa2a46f1520dd
pro-minhaj <parsonal322532@gmail.com>
2024-05-19 02:33:43 +0600

    Implement Header Section Responsive And Design Modifications

--- app/components/Shared/Header/Header.jsx
+++ app/components/Shared/Header/Header.jsx
@@ -4,6 +4,7 @@ import Image from 'next/image';
 import logo from '@/assets/images/logo.svg';
 import { FaHeart, FaUser } from "react-icons/fa6";
 import { FaShoppingCart } from "react-icons/fa";
+import { IoSearch } from "react-icons/io5";
 
 const Header = () => {
     // Header Items
@@ -27,6 +28,7 @@ const Header = () => {
             count: null
         },
     ]
+
     return (
         <header className="py-4 bg-white shadow-sm">
             <div className="container flex items-center justify-between mx-auto">
@@ -34,25 +36,29 @@ const Header = () => {
                     <Image width={150} height={100} src={logo} alt="Logo" priority className="w-32" />
                 </Link>
 
-                <div className="relative flex w-full max-w-xl">
-                    <span className="absolute text-lg text-gray-400 left-4 top-3">
-                        <i className="fa-solid fa-magnifying-glass"></i>
-                    </span>
-                    <input
-                        type="text"
-                        name="search"
-                        id="search"
-                        className="hidden w-full px-3 py-3 border border-r-0 border-primary rounded-l-md focus:outline-none md:flex"
-                        placeholder="Search..."
-                    />
-                    <button
-                        className="items-center hidden px-8 text-white transition border bg-primary border-primary rounded-r-md hover:bg-transparent hover:text-primary md:flex"
-                    >
-                        Search
-                    </button>
+                <div className="hidden w-full max-w-xl lg:block">
+                    <div className='flex'>
+                        <div className='flex items-center w-full px-3 py-1.5 border border-r-0 border-primary rounded-l-md'>
+                            <span className="text-lg text-gray-400">
+                                <IoSearch />
+                            </span>
+                            <input
+                                type="search"
+                                name="search"
+                                id="search"
+                                className="w-full border-transparent !ring-transparent !border-none outline-none !focus:border-none focus:outline-none"
+                                placeholder="Search..."
+                            />
+                        </div>
+                        <button
+                            className="items-center hidden px-8 text-white transition-colors border bg-primary border-primary rounded-r-md hover:bg-transparent hover:text-primary md:flex"
+                        >
+                            Search
+                        </button>
+                    </div>
                 </div>
 
-                <div className="flex items-center gap-5">
+                <div className="flex items-center gap-3 md:gap-5">
                     {/* Header Items */}
                     {
                         headerItems.map((item, index) => <Link key={index} href={`/${item.link}`} className="block">
@@ -60,7 +66,7 @@ const Header = () => {
                                 <span className='text-xl'>
                                     {item.icon}
                                 </span>
-                                <span className="text-xs leading-3">{item.name}</span>
+                                <span className="hidden text-xs leading-3 sm:block">{item.name}</span>
                                 {
                                     item.count && <span className="flex items-center justify-center w-5 h-5 text-xs text-white rounded-full bg-primary">
                                         {item.count}
