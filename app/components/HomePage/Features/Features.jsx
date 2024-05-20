7e3f231881e883726868e3e162c50846ccb7cd01
pro-minhaj <parsonal322532@gmail.com>
2024-05-21 03:58:43 +0600

    Implement Home Page Features Section convert multi lang

--- app/components/HomePage/Features/Features.jsx
+++ app/components/HomePage/Features/Features.jsx
@@ -2,27 +2,30 @@ import Image from 'next/image';
 import deliveryVanIcon from '@/assets/images/icons/delivery-van.svg';
 import moneyBackIcon from '@/assets/images/icons/money-back.svg';
 import serviceHoursIcon from '@/assets/images/icons/service-hours.svg';
+import { getDictionary } from '@/app/[lang]/dictionaries/dictionaries';
 
-// All Features Items
-const allFeaturesItems = [
-    {
-        title: "Free Shipping",
-        description: "Order over $200",
-        iconImg: deliveryVanIcon,
-    },
-    {
-        title: "Money Returns",
-        description: "30 days money returns",
-        iconImg: moneyBackIcon,
-    },
-    {
-        title: "24/7 Support",
-        description: "Customer support",
-        iconImg: serviceHoursIcon,
-    },
-]
+const Features = async ({ lang }) => {
+    const { home_page: { features_section } } = await getDictionary(lang);
 
-const Features = () => {
+    // All Features Items
+    const allFeaturesItems = [
+        {
+            title: features_section.all_features_items.feature_1.title,
+            description: features_section.all_features_items.feature_1.description,
+            iconImg: deliveryVanIcon,
+        },
+        {
+            title: features_section.all_features_items.feature_2.title,
+            description: features_section.all_features_items.feature_1.description,
+            iconImg: moneyBackIcon,
+        },
+        {
+            title: features_section.all_features_items.feature_3.title,
+            description: features_section.all_features_items.feature_1.description,
+            iconImg: serviceHoursIcon,
+        },
+    ]
+
     return (
         <div className="container py-10 md:py-16">
             <div className="grid justify-center w-10/12 grid-cols-1 gap-6 mx-auto md:grid-cols-3">
