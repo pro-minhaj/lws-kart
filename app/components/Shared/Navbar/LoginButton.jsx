d7fb598ee28708b620da82c8a5bf47b82e294e3d
pro-minhaj <parsonal322532@gmail.com>
2024-05-24 02:48:14 +0600

    Implement Logout Functions using next auth signout function

--- app/components/Shared/Navbar/LoginButton.jsx
+++ app/components/Shared/Navbar/LoginButton.jsx
@@ -1,19 +1,49 @@
 "use client";
+import { signOut, useSession } from "next-auth/react";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
+import ButtonLoading from "../../Loading/ButtonLoading/ButtonLoading";
+import { toast } from "sonner";
 
 const LoginButton = ({ dict }) => {
     const pathname = usePathname();
+    const { data, status } = useSession();
+
     const isLoginPage = pathname === "/login" || pathname === '/register';
+
     if (isLoginPage) {
         return null;
     }
+
+    if (status === "loading") {
+        return <ButtonLoading />;
+    }
+
+    const handleLogout = () => {
+        signOut()
+        toast.success("Logout successfully")
+    }
+
     return (
-        <div className="flex items-center">
-            <Link href="/login" className="text-gray-200 transition hover:text-white">{dict.login}</Link>
-            <span className="text-gray-200 transition hover:text-white">/</span>
-            <Link href="/register" className="text-gray-200 transition hover:text-white">{dict.register}</Link>
-        </div>
+        <>
+            {
+                status === "authenticated" ?
+                    <div className="flex items-center gap-2 text-white">
+                        <p>
+                            <small>{data?.user?.name}</small>
+                        </p>
+                        <button onClick={handleLogout} className="text-sm">
+                            logout
+                        </button>
+                    </div>
+                    :
+                    <div className="flex items-center">
+                        <Link href="/login" className="text-gray-200 transition hover:text-white">{dict.login}</Link>
+                        <span className="text-gray-200 transition hover:text-white">/</span>
+                        <Link href="/register" className="text-gray-200 transition hover:text-white">{dict.register}</Link>
+                    </div>
+            }
+        </>
     );
 };
 
