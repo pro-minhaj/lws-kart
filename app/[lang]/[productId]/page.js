47981177474709405cfa5b74295a99cdb0f21d15
pro-minhaj <parsonal322532@gmail.com>
2024-05-24 18:24:08 +0600

    Implement Product Details Page related product Section Design Complete

--- app/[lang]/[productId]/page.js
+++ app/[lang]/[productId]/page.js
@@ -8,7 +8,8 @@ import {
     FaInstagram,
     FaHeart,
     FaShoppingBag,
-    FaChevronRight
+    FaChevronRight,
+    FaSearch
 } from 'react-icons/fa';
 
 // import all Images
@@ -202,6 +203,64 @@ const ProductsDetailsPage = ({ params: { lang, productId } }) => {
                 </div>
             </div>
             {/*description*/}
+
+            {/* related product */}
+            <div className='container pb-16'>
+                <h2 className='mb-6 text-2xl font-medium text-gray-800 uppercase'>
+                    Related products
+                </h2>
+                <div className='grid grid-cols-4 gap-6'>
+                    {/* Product 1 */}
+                    <div className='overflow-hidden bg-white rounded shadow group'>
+                        <div className='relative'>
+                            <Image
+                                src={product1}
+                                alt='product 1'
+                                className='w-full'
+                                width={300}
+                                height={200}
+                            />
+                            <div className='absolute inset-0 flex items-center justify-center gap-2 transition bg-black opacity-0 bg-opacity-40 group-hover:opacity-100'>
+                                <button
+                                    className='flex items-center justify-center h-8 text-lg text-white transition rounded-full w-9 bg-primary hover:bg-gray-800'
+                                    title='view product'
+                                >
+                                    <FaSearch />
+                                </button>
+                                <button
+                                    className='flex items-center justify-center h-8 text-lg text-white transition rounded-full w-9 bg-primary hover:bg-gray-800'
+                                    title='add to wishlist'
+                                >
+                                    <FaHeart />
+                                </button>
+                            </div>
+                        </div>
+                        <div className='px-4 pt-4 pb-3'>
+                            <a href='#'>
+                                <h4 className='mb-2 text-xl font-medium text-gray-800 uppercase transition hover:text-primary'>
+                                    Guyer Chair
+                                </h4>
+                            </a>
+                            <div className='flex items-baseline mb-1 space-x-2'>
+                                <p className='text-xl font-semibold text-primary'>$45.00</p>
+                                <p className='text-sm text-gray-400 line-through'>$55.90</p>
+                            </div>
+                            <div className='flex items-center'>
+                                <div className='flex gap-1 text-sm text-yellow-400'>
+                                    {[...Array(5)].map((_, index) => (
+                                        <FaStar key={index} />
+                                    ))}
+                                </div>
+                                <div className='ml-3 text-xs text-gray-500'>(150)</div>
+                            </div>
+                        </div>
+                        <button className='block w-full py-1 text-center text-white transition border rounded-b bg-primary border-primary hover:bg-transparent hover:text-primary'>
+                            Add to cart
+                        </button>
+                    </div>
+                </div>
+            </div>
+            {/* related product */}
         </div>
     );
 };
